1ac9a4ca2ea1fb4be98702a9cff870e1775e7338
Letícia <virgilioleticia240902@gmail.com>
2024-08-22 15:12:06 -0300

    React Playground

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Tarefa from './pages/tarefa/Tarefa'
+
+function App() {
+
+  return (
+    <>
+        {/*<Home 
+        titulo = "Minha primeira postagem"
+        texto = "Texto da minha primeira postagem"
+        />
+
+      <Home
+        titulo="Minha segunda postagem"
+        texto="Texto da minha segunda postagem"
+      /> */}
+
+      {/* <Contador /> 
+
+       <contadorClasses /> */}
+
+      <Tarefa/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,27 @@
+﻿import { useState } from "react"
+
+function Contador() {
+
+  // const [nome_estado, função_setState] = useState(vale_inicial_do_estado)
+
+  //Estado valor (React)
+  const [valor, setValor] = useState (0);
+
+  //Function (TypesCript)
+  function handleClick(){
+    setValor(valor +1);
+  }
+
+  return (
+    <div>
+      <h2>Componente Contador</h2>
+
+      <p> O valor atual do meu estato é: {valor} </p>
+
+      <button onClick={handleClick}>Adicionar +1</button>
+
+      </div>
+  )
+}
+
+export default Contador
